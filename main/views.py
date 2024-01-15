c249786a24d1ef3bd486f20cd0f6911fb77d96ad
BermudoMan <ivan.kurochkin.95@bk.ru>
2024-01-15 19:44:45 +0300

    add carousel

--- main/views.py
+++ main/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from .models import GalleryItem
 
 def index(request):
-    items = GalleryItem.objects.values_list('way')
-    return render(request, 'main/main.html', {'items': items})
+    items = GalleryItem.objects.values('title')
+    list_items = list(items)
+    corr_items = []
+    for i in list_items:
+        j = i.values()
+        j = str(j)
+        j = j.replace("dict_values(['", "static/main/img/")
+        j = j.replace("'])", '.jpg')
+        corr_items.append(j)
+    return render(request, 'main/main.html', {'items': corr_items})
 
 def about(request):
     return render(request, 'main/about.html')
